2f479fdd0d113a58d863a335714f68921c711cec
Mccel Villanueva MacOS <mccel.villanueva1103@gmail.com>
2025-09-06 16:47:12 +0800

    Frontend-Backend Connected using axios

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,10 +4,10 @@ import { Ratelimit } from '@upstash/ratelimit'; // for rate limiting
 import dotenv from 'dotenv';
 dotenv.config();
 
-//10 requests per 20 seconds
+//100 requests per 60 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, '60 s'),
+    limiter: Ratelimit.slidingWindow(100, '60 s'),
 })
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from "dotenv";
+import cors from 'cors';
+
 import notesRouter from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
-import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config(); 
@@ -9,16 +11,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
-
 //middleware 
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
 app.use(express.json()); //to parse JSON bodies
 app.use(rateLimiter); //apply rate limiting middleware
-
+ 
 app.use("/api/notes", notesRouter);
 
 connectDB().then( () => {
     app.listen(PORT, () => {
         console.log("Server started on port", PORT);
     }); 
-})
+}) 
